f13c0d9ee308b24900fa67c1b679cc4bdca57464
Dilraj <djrandhawa15@hotmail.com>
2025-09-05 13:16:31 -0700

    lab1: bun + hono minimal server with routes

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+// server/index.ts
+import { app } from './app'
+
+// Prefer PORT from env; default 3000
+const port = Number(process.env.PORT || 3000)
+
+export default {
+  port,
+  fetch: app.fetch,
+}
+
+console.log(`🚀 Server running on http://localhost:${port}`)
+
